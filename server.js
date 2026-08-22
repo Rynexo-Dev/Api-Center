3ada9868b9c4ad7578fc3ffd52e87c80f538ff57
Rynexo Dev <banbari887@gmail.com>
2026-08-21 17:56:19 -1200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-import express from 'express'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import Navbar from './src/Routes/Navbar.route.js'
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-const app = express()
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import Navbar from "./src/Routes/Navbar.route.js";
 
-app.get('/', (req, res) => {
-  res.sendFile('/src/templates/index.html', {root: __dirname})
-}).get('/css/style.css', (req, res) => {
-  res.sendFile('/src/templates/style.css', {root: __dirname})
-})
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const templatesPath = path.join(__dirname, "src", "templates");
+
+app.use(express.static(templatesPath));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(templatesPath, "index.html"));
+});
 
 app.use("/apis", Navbar);
 
